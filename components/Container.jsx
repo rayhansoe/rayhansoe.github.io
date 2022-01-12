92091a5fbdefc9ee53834745330533d911a9a2fb
bukanrayhan <rayhan.rhssoe2@gmail.com>
2022-01-12 19:22:54 +0700

    mobile first container.

--- components/Container.jsx
+++ components/Container.jsx
@@ -1,5 +1,5 @@
 const Container = ({ children }) => {
-	return <div className='container mx-auto px-8 sm:px-4'>{children}</div>
+	return <div className='container mx-auto px-4 md:px-8'>{children}</div>
 }
 
 export default Container
