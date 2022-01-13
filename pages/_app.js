9a9ee5c02123310c8a7463a80aff7912daaa05b6
bukanrayhan <rayhan.rhssoe2@gmail.com>
2022-01-13 17:50:18 +0700

    move BGimg to _app, and wrap the pages using layout.

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
 import '../styles/globals.scss'
 
 import { ThemeProvider } from 'next-themes'
+import dynamic from 'next/dynamic'
+
+const Layout = dynamic(() => import('../components/Layout'))
+const BGimg = dynamic(() => import('../components/BGimg'))
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<ThemeProvider enableSystem={true} attribute='class'>
-			<Component {...pageProps} />
+			<BGimg />
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
 		</ThemeProvider>
 	)
 }
--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,8 @@
 // import Image from 'next/image'
 import dynamic from 'next/dynamic'
 
-const BGimg = dynamic(() => import('../components/BGimg'))
-const NavBar = dynamic(() => import('../components/NavBar'))
+const Hero = dynamic(() => import('../components/Hero'))
 
 export default function Home() {
-	return (
-		<body className='text-text-dark dark:text-text-light'>
-			<BGimg />
-			<NavBar />
-		</body>
-	)
+	return <Hero />
 }
