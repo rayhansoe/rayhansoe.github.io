46c705770f34ab23948b9cbdf478a3dafd0317fe
bukanrayhan <rayhan.rhssoe2@gmail.com>
2022-01-17 16:59:09 +0700

    add title.

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,24 @@ import '../styles/globals.scss'
 
 import { ThemeProvider } from 'next-themes'
 import dynamic from 'next/dynamic'
+import Head from 'next/head'
 
 const Layout = dynamic(() => import('../components/Layout'))
 const BGimg = dynamic(() => import('../components/BGimg'))
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<ThemeProvider enableSystem={true} attribute='class'>
-			<BGimg />
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</ThemeProvider>
+		<>
+			<Head>
+				<title>Rayhan Soe | Front End Developer</title>
+			</Head>
+			<ThemeProvider enableSystem={true} attribute='class'>
+				<BGimg />
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</ThemeProvider>
+		</>
 	)
 }
 
