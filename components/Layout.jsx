5837a515ee5d9e6d6658c26ed7529a474046402b
bukanrayhan <rayhan.rhssoe2@gmail.com>
2022-01-18 16:06:28 +0700

    add overflow-x-hidden.

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,7 +5,7 @@ const Footer = dynamic(() => import('./Footer'))
 
 const Layout = ({ children }) => {
 	return (
-		<div className='flex flex-col justify-between min-h-screen'>
+		<div className='flex flex-col justify-between min-h-screen overflow-x-hidden'>
 			<NavBar />
 			{children}
 			<Footer />
