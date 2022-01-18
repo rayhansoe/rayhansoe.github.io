b15d7960b46d42992cb257ede68bce9e3dc6ec34
bukanrayhan <rayhan.rhssoe2@gmail.com>
2022-01-18 16:05:01 +0700

    add new breakpoints.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
 	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	darkMode: 'class',
 	theme: {
+		screens: {
+			'2xs': '375px',
+			xs: '480px',
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+			'2xl': '1536px',
+		},
 		extend: {
 			zIndex: {
 				0: '0',
