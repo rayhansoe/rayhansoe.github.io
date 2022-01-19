0d86aba8bbf58cb68a63d9a2fb5cf33df3a7d162
bukanrayhan <rayhan.rhssoe2@gmail.com>
2022-01-19 17:36:58 +0700

    text color 2 modes.

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,7 +8,7 @@ class MyDocument extends Document {
 
 	render() {
 		return (
-			<Html>
+			<Html className='text-text-light dark:text-text-dark'>
 				<Head>
 					<link rel='preconnect' href='https://fonts.googleapis.com' />
 					<link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='' />
@@ -17,7 +17,7 @@ class MyDocument extends Document {
 						rel='stylesheet'
 					/>
 				</Head>
-				<body>
+				<body className='text-text-light dark:text-text-dark'>
 					<Main />
 					<NextScript />
 				</body>
