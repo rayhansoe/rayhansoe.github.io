778f8f1d49f749baa3f2ba01cc77f4dd24a69c4f
bukanrayhan <rayhan.rhssoe2@gmail.com>
2022-01-21 18:50:15 +0700

    Error 404 Page.

---
+++ pages/404.jsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+import Error from '../public/404 Error with a cute animal-bro.webp'
+export default function Custom404() {
+	return (
+		<div className='relative w-full self-center justify-self-center md:w-2/3 lg:w-1/2 2xl:w-2/5'>
+			<Image src={Error} alt='Error' width='2000' height='2000' layout='responsive' quality={100} />
+		</div>
+	)
+}
