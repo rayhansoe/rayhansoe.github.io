4f33bdcd2520cfff6268f8c2ca351bc9ee0dec2e
bukanrayhan <rayhan.rhssoe2@gmail.com>
2022-01-21 22:27:33 +0700

    add new things on footer.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,4 +1,5 @@
 import dynamic from 'next/dynamic'
+import Link from 'next/link'
 
 const Container = dynamic(() => import('./Container'))
 
@@ -6,8 +7,28 @@ const Footer = () => {
 	return (
 		<section className='py-6'>
 			<Container>
-				<footer className='flex items-center justify-center'>
+				<footer className='flex flex-col items-center justify-center'>
 					<h4 className='font-averia'>Made with ❤️ in Indonesia.</h4>
+					<h4 className='font-averia'>
+						Develop use{' '}
+						<Link href='https://nextjs.org/'>
+							<a target='_blank' className='text-primary-color hover:underline'>
+								Next Js
+							</a>
+						</Link>
+						,{' '}
+						<Link href='tailwindcss.com'>
+							<a target='_blank' className='text-primary-color hover:underline'>
+								Tailwind CSS
+							</a>
+						</Link>{' '}
+						and Hosted on{' '}
+						<Link href='https://vercel.com/'>
+							<a target='_blank' className='text-primary-color hover:underline'>
+								Vercel
+							</a>
+						</Link>
+					</h4>
 				</footer>
 			</Container>
 		</section>
