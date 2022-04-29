4d15407da827c8d7256fb05b82dd86e94c3e3b2d
bukanrayhan <rayhan.rhssoe2@gmail.com>
2022-04-29 15:21:13 +0700

    .

--- components/Bar.jsx
+++ components/Bar.jsx
@@ -1,7 +1,7 @@
 export const Bar = ({ animationDuration, progress }) => {
 	return (
 		<div
-			className='fixed left-0 top-0 z-50 h-[3px] w-full bg-primary-color'
+			className='fixed left-0 top-0 z-50 h-1 w-full bg-primary-color'
 			style={{
 				marginLeft: `${(-1 + progress) * 100}%`,
 				transition: `margin-left ${animationDuration}ms linear`,
